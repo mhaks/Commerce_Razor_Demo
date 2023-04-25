5814a06ece569e6e18b4da259decd3ca10a288bd
mhaks <mike.hakanson@gmail.com>
2023-04-25 10:38:46 -0600

    shopping cart component/scaffold

---
+++ CommerceRazorDemo/Pages/Shared/Components/CartStatus.cs
@@ -0,0 +1,29 @@
+﻿using CommerceRazorDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceRazorDemo.Pages.Shared.Components
+{
+    public class CartStatusViewComponent : ViewComponent
+    {
+        private readonly CommerceRazorDemo.Data.CommerceRazorDemoContext _context;
+        private readonly ILogger<IndexModel> _logger;
+
+        public CartStatusViewComponent(CommerceRazorDemoContext context, ILogger<IndexModel> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            // TODO 
+            var orders = await _context.Order
+                .Where(x => x.CustomerId == 0)
+                .AsNoTracking()
+                .ToListAsync();
+
+            return View(false);
+        }
+    }
+}
