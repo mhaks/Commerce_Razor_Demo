374e22c3f8c9da1548e4d89d1a92816f18b55dfa
mhaks <mike.hakanson@gmail.com>
2023-05-31 16:34:02 -0600

    user switch component

---
+++ CommerceRazorDemo/Pages/Shared/Components/UserSwitch.cs
@@ -0,0 +1,38 @@
+﻿using CommerceRazorDemo.Data;
+using CommerceRazorDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace CommerceRazorDemo.Pages.Shared.Components
+{
+    public class UserSwitchViewComponent : ViewComponent
+    {
+        public UserSwitchViewComponent()
+        {
+
+        }
+
+
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var context = ViewData["DbContext"] as CommerceRazorDemo.Data.CommerceRazorDemoContext;
+
+            SelectList users = null;
+            if (context != null)
+            {
+                users = new SelectList(await context.Customer.OrderBy(c => c.LastName).ToListAsync(), "Id", "FullName");
+            }
+            
+            
+            
+            return View(users);
+        }
+
+
+     
+    }
+}
