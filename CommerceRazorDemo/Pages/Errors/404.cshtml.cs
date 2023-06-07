2cc96a6ce1073c08d9375ea0bb08e0849d112fc4
mhaks <mike.hakanson@gmail.com>
2023-06-07 17:40:55 -0600

    404 custom error page

---
+++ CommerceRazorDemo/Pages/Errors/404.cshtml.cs
@@ -0,0 +1,17 @@
+using CommerceRazorDemo.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CommerceRazorDemo.Pages.Errors
+{
+    public class _404Model : CommerceDemoPageModel
+    {
+        public _404Model(CommerceRazorDemoContext context, ILogger<CommerceDemoPageModel> logger) : base(context, logger)
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
--- CommerceRazorDemo/Program.cs
+++ CommerceRazorDemo/Program.cs
@@ -33,7 +33,11 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -50,5 +54,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.UseStatusCodePagesWithRedirects("/errors/{0}");
 
 app.Run();
